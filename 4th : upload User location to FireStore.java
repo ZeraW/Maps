29799bb96b90b5c06954b7c06d123105505d8993
ZeraW <hema_eie552@yahoo.com>
2019-02-15 18:06:48 +0200

    Update and rename 4th : get last known location to 4th : upload User location to FireStore.java

---
+++ 4th : upload User location to FireStore.java
@@ -0,0 +1,250 @@
+1st: you need the following 
+------------------------------------------------------------------------------------------------------------
+a- User model class
+b- userLocation model class 
+i've add them at the end of this File
+------------------------------------------------------------------------------------------------------------
+
+2nd : in string.xml :
+--------------------------------
+//this is the best practice for collection name in firestore
+<string name="collection_user_locations">User Locations</string>
+<string name="collection_users">Users</string>
+------------------------------------------------------------------------------------------------------------
+
+3rd : in main activity : 
+--------------------------------
+private FusedLocationProviderClient mFusedLocationClient;
+private FirebaseFirestore mDb;
+private UserLocation mUserLocation; // the model class
+
+
+oncreate(){
+mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+mDb = FirebaseFirestore.getInstance();
+}
+
+private void getUserDetails(){
+        if(mUserLocation == null){
+            mUserLocation = new UserLocation();
+            DocumentReference userRef = mDb.collection(getString(R.string.collection_users))
+                    .document(FirebaseAuth.getInstance().getUid());
+
+            userRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if(task.isSuccessful()){
+                        Log.d(TAG, "onComplete: successfully set the user client.");
+                        User user = task.getResult().toObject(User.class);
+                        mUserLocation.setUser(user);
+                        getLastKnownLocation();
+                    }
+                }
+            });
+        }
+        else{
+            getLastKnownLocation();
+        }
+}
+
+private void getLastKnownLocation() {
+        Log.d(TAG, "getLastKnownLocation: called.");
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mFusedLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<android.location.Location>() {
+            @Override
+            public void onComplete(@NonNull Task<android.location.Location> task) {
+                if (task.isSuccessful()) {
+                    Location location = task.getResult();
+                    GeoPoint geoPoint = new GeoPoint(location.getLatitude(), location.getLongitude());
+                    mUserLocation.setGeo_point(geoPoint);  //model attribute 
+                    mUserLocation.setTimestamp(null);   //model attribute
+                    saveUserLocation();
+                }
+            }
+        });
+
+}
+
+private void saveUserLocation(){
+
+        if(mUserLocation != null){
+            DocumentReference locationRef = mDb
+                    .collection(getString(R.string.collection_user_locations))
+                    .document(FirebaseAuth.getInstance().getUid());
+
+            locationRef.set(mUserLocation).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if(task.isSuccessful()){
+                        Log.d(TAG, "saveUserLocation: \ninserted user location into database." +
+                                "\n latitude: " + mUserLocation.getGeo_point().getLatitude() +
+                                "\n longitude: " + mUserLocation.getGeo_point().getLongitude());
+                    }
+                }
+            });
+        }
+}
+    
+------------------------------------------------------------------------------------------------------------
+4th : how to use it 
+---------------------
+you can call this method 'getUserDetails()' after getting permission with doSomething(); to get and upload the userlocation
+    
+
+
+
+------------------------------------------------------------------------------------------------------------
+Models : i don't think the Parcelable is necessary 
+---------------------------------------------------
+public class User implements Parcelable{
+
+    private String email;
+    private String user_id;
+    private String username;
+    private String avatar;
+
+    public User(String email, String user_id, String username, String avatar) {
+        this.email = email;
+        this.user_id = user_id;
+        this.username = username;
+        this.avatar = avatar;
+    }
+
+    public User() {
+
+    }
+
+    protected User(Parcel in) {
+        email = in.readString();
+        user_id = in.readString();
+        username = in.readString();
+        avatar = in.readString();
+    }
+
+    public static final Creator<User> CREATOR = new Creator<User>() {
+        @Override
+        public User createFromParcel(Parcel in) {
+            return new User(in);
+        }
+
+        @Override
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public static Creator<User> getCREATOR() {
+        return CREATOR;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "email='" + email + '\'' +
+                ", user_id='" + user_id + '\'' +
+                ", username='" + username + '\'' +
+                ", avatar='" + avatar + '\'' +
+                '}';
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(email);
+        dest.writeString(user_id);
+        dest.writeString(username);
+        dest.writeString(avatar);
+    }
+}
+------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------
+public class UserLocation {
+
+    private User user;
+    private GeoPoint geo_point;
+    private @ServerTimestamp Date timestamp;
+
+    public UserLocation(User user, GeoPoint geo_point, Date timestamp) {
+        this.user = user;
+        this.geo_point = geo_point;
+        this.timestamp = timestamp;
+    }
+
+    public UserLocation() {
+
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public GeoPoint getGeo_point() {
+        return geo_point;
+    }
+
+    public void setGeo_point(GeoPoint geo_point) {
+        this.geo_point = geo_point;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return "UserLocation{" +
+                "user=" + user +
+                ", geo_point=" + geo_point +
+                ", timestamp=" + timestamp +
+                '}';
+    }
+
+}
